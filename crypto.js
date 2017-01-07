4398f47d4d46fa4357c74930292c7d53ea44b944
Jacob Cornwell <jpcornwell@users.noreply.github.com>
2017-01-06 22:16:28 -0600

    Work on hex and ascii encoding/decoding

--- crypto.js
+++ crypto.js
@@ -1,17 +1,50 @@
 
-
 function hexDecode(hex) {
-    var bytes = '';
+    if (hex.length % 2 === 1) {
+        throw 'hex string is not even length';
+    }
+
+    var bytes = new Uint8Array(hex.length / 2);
 
     for (var i = 0; i < hex.length; i += 2) {
-       bytes += String.fromCharCode((parseInt(hex.substr(i, 2), 16)));
+       bytes[i/2] = parseInt(hex.substr(i, 2), 16);
     }
     
     return bytes;
 }
 
 function hexEncode(bytes) {
+    var hex = '';
 
+    for (var i = 0; i < bytes.length; i++) {
+        var hexByte = bytes[i].toString(16);
+        if (hexByte.length === 1) {
+            hexByte = '0' + hexByte;
+        }
+        hex += hexByte;
+    }
+
+    return hex;
+}
+
+function asciiEncode(bytes) {
+    var ascii = '';
+
+    for (var i = 0; i < bytes.length; i++) {
+        ascii += String.fromCharCode(bytes[i]);
+    }
+
+    return ascii;
+}
+
+function asciiDecode(ascii) {
+    var bytes = new Uint8Array(ascii.length);
+
+    for (var i = 0; i < ascii.length; i++) {
+       bytes[i] = ascii.charAt(i).charCodeAt();
+    }
+    
+    return bytes;
 }
 
 // need to replace btoa/atob with buffer.toString or use my own encoding logic
