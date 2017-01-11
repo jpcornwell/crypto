36679aa649e325260802b6aaca788cb2fca4ff22
Jacob Cornwell <jpcornwell@users.noreply.github.com>
2017-01-10 20:49:32 -0600

    Add method to score english text (probably incorrect)

--- crypto.js
+++ crypto.js
@@ -1,4 +1,51 @@
 
+var englishFreq = {
+    'a': 0.08167, 'b': 0.01492, 'c': 0.02782, 'd': 0.04253, 'e': 0.12702, 
+    'f': 0.02228, 'g': 0.02015, 'h': 0.06094, 'i': 0.06966, 'j': 0.00153, 
+    'k': 0.00772, 'l': 0.04025, 'm': 0.02406, 'n': 0.06749, 'o': 0.07507, 
+    'p': 0.01929, 'q': 0.00095, 'r': 0.05987, 's': 0.06327, 't': 0.09056, 
+    'u': 0.02758, 'v': 0.00978, 'w': 0.02360, 'x': 0.00150, 'y': 0.01974, 
+    'z': 0.00074, ' ': 0.12702
+};
+
+var a = 'hello how are you today';
+var b = 'kdaf;lkdsdjaf kjdslaeio';
+var c = 'as of javascript you can use object keys';
+
+console.log(scoreEnglishText(a));
+console.log(scoreEnglishText(b));
+console.log(scoreEnglishText(c));
+
+function scoreEnglishText(text) {
+    text = text.toLowerCase();
+
+    // initialize counts
+    var counts = {};
+    for (var i = 97; i <= 122; i++) {
+        var char = String.fromCharCode(i);
+        counts[char] = 0;
+    }
+    counts[' '] = 0;
+    
+    // count characters
+    for (var i = 0; i < text.length; i++) {
+        var char = text[i];
+        // if char is a-z or space character
+        if ((char >= 97 && char <= 122) || char === 32) {
+            counts[char] += 1;
+        }
+    }
+
+    // calculate score
+    var score = 0;
+    Object.keys(englishFreq).forEach(function (char) {
+        var expected = englishFreq[char] * text.length;
+        score += ((counts[char] - expected)^2 / expected);
+    });
+
+    return score;
+}
+
 function fixedXor(a, b) {
     if (a.length !== b.length) {
         throw 'inputs are not equal length';
